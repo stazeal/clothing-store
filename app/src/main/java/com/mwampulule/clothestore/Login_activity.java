b2158bf89fca03791afef2d58e543b7d0f370f4d
stazeal <peacestazeal@gmail.com>
2024-12-01 22:45:50 +0300

    additional

--- app/src/main/java/com/mwampulule/clothestore/Login_activity.java
+++ app/src/main/java/com/mwampulule/clothestore/Login_activity.java
@@ -1,6 +1,11 @@
 package com.mwampulule.clothestore;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Login_activity extends AppCompatActivity {
+    EditText etEmail,etPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,30 @@ public class Login_activity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        etEmail = findViewById(R.id.EmailAddressL);
+        etPassword= findViewById(R.id.TextPassword);
+        btnLogin = findViewById(R.id.button2);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email= etEmail.getText().toString().trim();
+                String Password = etPassword.getText().toString().trim();
+
+                if (email.isEmpty()||Password.isEmpty()){
+                    Toast.makeText(Login_activity.this,"please fill all fields", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(Login_activity.this, "Welcome to Dashboard", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(Login_activity.this,DashboardActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/mwampulule/clothestore/Register_acitityy.java
+++ app/src/main/java/com/mwampulule/clothestore/Register_acitityy.java
@@ -1,6 +1,11 @@
 package com.mwampulule.clothestore;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Register_acitityy extends AppCompatActivity {
+    EditText etUserName,etEmail,etPassword;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,30 @@ public class Register_acitityy extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        etUserName = findViewById(R.id.TextText);
+        etEmail = findViewById(R.id.EmailAddress);
+        etPassword = findViewById(R.id.Password);
+        btnRegister = findViewById(R.id.button);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUserName.getText().toString().trim();
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                if (username.isEmpty()||email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(Register_acitityy.this, "please fill in all field",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(Register_acitityy.this, "Please login here", Toast.LENGTH_SHORT).show();
+                }
+
+                Intent intent = new Intent(Register_acitityy.this,Login_activity.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
